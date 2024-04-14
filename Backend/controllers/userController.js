126a17b9cffc96ca698714622b7916d605195706
akauffy <aak105@pitt.edu>
2024-04-14 11:01:06 -0400

    Added jwt and middleware for it

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,12 +1,15 @@
 const User = require("../models/userModel");
-
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 //login
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.login(email, password);
 
-        res.status(200).json({ email, user });
+
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ email, user, accessToken });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
@@ -24,4 +27,20 @@ const signupUser = async (req, res) => {
     }
 };
 
-module.exports = { signupUser, loginUser };
+// Endpoint for token refresh
+const refreshToken = async (req, res) => {
+    const refreshToken = req.body.refreshToken;
+  
+    // Verify refresh token
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403); // Forbidden
+  
+      // Generate new access token
+      const accessToken = jwt.sign({ userId: user.userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  
+      // Return new access token
+      res.json({ accessToken });
+    });
+  };
+
+module.exports = { signupUser, loginUser, refreshToken };
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const {signupUser, loginUser} = require("../controllers/userController");
+const {signupUser, loginUser, refreshToken} = require("../controllers/userController");
 
 router.post("/login", loginUser);
 
-router.post("signup", signupUser);
+router.post("/signup", signupUser);
+
+router.post("/refreshToken", refreshToken);
 
 module.exports = router;
