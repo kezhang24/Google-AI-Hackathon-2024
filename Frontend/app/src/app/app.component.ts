602a668d6ce9c692fedbcf6105277fe06baadc58
akauffy <aak105@pitt.edu>
2024-04-14 13:32:12 -0400

    Logout and bug

--- Frontend/app/src/app/app.component.ts
+++ Frontend/app/src/app/app.component.ts
@@ -20,17 +20,23 @@ export class AppComponent {
   title = 'app';
 
   ngOnInit(): void {
-    this.http.get<{user: User}>('http://localhost:4000/api/user/current-user').subscribe(response => {
-      const currentUser: LoginResponse = {
-        email: response.user.email,
-        user: response.user,
-        accessToken: localStorage['token']
+    this.http.get<{user: User}>('http://localhost:4000/api/user/current-user').subscribe({
+      next: response => {
+        const currentUser: LoginResponse = {
+          email: response.user.email,
+          user: response.user,
+          accessToken: localStorage['token']
+        }
+        this.authService.currentUserSig.set(currentUser);
+      },
+      error: () => {
+        this.authService.currentUserSig.set(null);
       }
-      this.authService.currentUserSig.set(currentUser);
     });
   }
 
   logout(): void {
-    localStorage.clear();
+    localStorage.setItem('token', '')
+    this.authService.currentUserSig.set(null);
   }
 }
